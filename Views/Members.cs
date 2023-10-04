795c05b76bccefa19c6bd73522d5303e1ea2532f
Dheerajraj123 <146334464+Dheerajraj123@users.noreply.github.com>
2023-10-04 19:01:16 +0500

    Add files via upload

---
+++ Views/Members.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace TennisCourtMembershipSoft.Models
+{
+    public class Members
+    {
+        public int MemberId { get; set; }
+
+        public string eventname { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string PasswordHash { get; set; }
+
+        // Add other member-specific properties here
+    }
+}
