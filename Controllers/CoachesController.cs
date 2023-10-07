481d5a492e6bbaea379188f473036e90ab1693c3
Alex <alex.phan2905@gmail.com>
2023-10-08 02:54:32 +0930

    Members list and coaches list with authorization

---
+++ Controllers/CoachesController.cs
@@ -0,0 +1,23 @@
+﻿using AnyoneForTennis.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnyoneForTennis.Controllers
+{
+    public class CoachesController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CoachesController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var members = _userManager.GetUsersInRoleAsync("Coach").Result;
+
+            return View(members);
+        }
+    }
+}
---
+++ Controllers/MembersController.cs
@@ -0,0 +1,26 @@
+﻿using AnyoneForTennis.Data;
+using AnyoneForTennis.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnyoneForTennis.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class MembersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public MembersController(UserManager<ApplicationUser> userManager)
+        { 
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var members = _userManager.GetUsersInRoleAsync("Member").Result;
+
+            return View(members);
+        }
+    }
+}
